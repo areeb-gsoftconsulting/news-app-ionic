3ff9363913410df48ac1e8ce62144a4bc78b87f4
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2023-07-25 18:29:01 +0500

    feat: formating card

--- src/components/CardsContainer/index.tsx
+++ src/components/CardsContainer/index.tsx
@@ -1,16 +1,21 @@
 import {
+  IonAvatar,
   IonCard,
   IonCardContent,
   IonCardHeader,
   IonCardSubtitle,
   IonCardTitle,
+  IonChip,
   IonCol,
   IonContent,
   IonGrid,
+  IonIcon,
+  IonLabel,
   IonRow,
   IonTitle,
 } from "@ionic/react";
 import React from "react";
+import { saveSharp } from "ionicons/icons";
 
 const CardsContainer: React.FC = () => {
   let cards = [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
@@ -19,7 +24,14 @@ const CardsContainer: React.FC = () => {
       <IonRow>
         {cards.map((data, index) => {
           return (
-            <IonCol sizeXs="12" sizeSm="6" sizeMd="4" sizeLg="4" sizeXl="2">
+            <IonCol
+              key={index}
+              sizeXs="12"
+              sizeSm="6"
+              sizeMd="4"
+              sizeLg="4"
+              sizeXl="2"
+            >
               <IonCard>
                 <img
                   alt="Silhouette of mountains"
@@ -27,13 +39,37 @@ const CardsContainer: React.FC = () => {
                 />
                 <IonCardHeader>
                   <IonCardTitle>Card Title</IonCardTitle>
-                  <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
                 </IonCardHeader>
 
                 <IonCardContent>
                   Here's a small text description for the card content. Nothing
                   more, nothing less.
                 </IonCardContent>
+                <IonRow class="ion-justify-content-between">
+                  <IonRow
+                    style={{
+                      paddingBottom: 0,
+                    }}
+                    class="ion-justify-content-between ion-align-items-center ion-padding"
+                  >
+                    <IonAvatar style={{ height: 20, width: 20 }}>
+                      <img
+                        alt="Silhouette of a person's head"
+                        src="https://ionicframework.com/docs/img/demos/avatar.svg"
+                      />
+                    </IonAvatar>
+                    <p>some</p>
+                    <p>some</p>
+                  </IonRow>
+                  <IonRow
+                    style={{
+                      paddingBottom: 0,
+                    }}
+                    class="ion-align-items-center ion-padding"
+                  >
+                    <IonIcon icon={saveSharp} />
+                  </IonRow>
+                </IonRow>
               </IonCard>
             </IonCol>
           );
