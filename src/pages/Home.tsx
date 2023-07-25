3e24610f35a3321c6536cf9b2c1a6acab74d0f99
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2023-07-25 17:14:58 +0500

    feat: redux added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { IonPage } from "@ionic/react";
 import "./Home.css";
 import MenuComp from "../components/Menu";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 
 const Home: React.FC = () => {
   return (
