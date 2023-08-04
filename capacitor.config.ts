531d03096c60272715c4ec9e445df1eca35aa677
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2023-08-04 10:37:16 +0500

    feat: run on android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'newsapp',
-  webDir: 'dist',
+  appId: "io.ionic.starter",
+  appName: "newsapp",
+  webDir: "dist",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "http",
+  },
 };
 
 export default config;
