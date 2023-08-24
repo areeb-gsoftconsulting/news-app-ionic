88274ed37c8182e4cb05d9915d828095249b8408
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2023-08-24 11:03:14 +0500

    feat: auto deployment

--- dist/assets/ios.transition-legacy-1990c3ad.js
+++ dist/assets/ios.transition-legacy-1990c3ad.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-04f9703b.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
+System.register(["./index-legacy-8ad7402c.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
