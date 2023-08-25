e33d6e51d695fea4cded5653ce2495e59e593a10
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2023-08-25 11:59:19 +0500

    feat: setup circleci

--- dist/assets/ios.transition-legacy-e1a960e4.js
+++ dist/assets/ios.transition-legacy-e1a960e4.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-8ad7402c.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
+System.register(["./index-legacy-10794990.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- dist/assets/swipe-back-legacy-5177157f.js
+++ dist/assets/swipe-back-legacy-5177157f.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-8ad7402c.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-10794990.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
